3ccdc28ea8391266ef11e4b7d4c7834e2d5fe7c5
Martin Alejandro Bonari <131064127+martinmab2@users.noreply.github.com>
2024-06-29 02:45:41 -0300

    Se aplicó lombok y se agregó los packages DTO y Mapper para Materia

---
+++ tp4_MartinAlejandroBonari/src/main/java/ar/edu/unju/fi/mapper/MateriaMapper.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.mapper;
+
+import java.util.List;
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import ar.edu.unju.fi.dto.MateriaDTO;
+import ar.edu.unju.fi.model.Materia;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface MateriaMapper {
+	@Mapping(source = "codigo", target = "codigo")
+	@Mapping(source = "nombre", target = "nombre")
+	@Mapping(source = "curso", target = "curso")
+	@Mapping(source = "cantidadHoras", target = "cantidadHoras")
+	@Mapping(source = "modalidad", target = "modalidad")
+	@Mapping(source = "docente", target = "docente")
+	@Mapping(source = "carrera", target = "carrera")
+	
+	MateriaDTO toMateriaDTO(Materia materia);
+	
+	@InheritConfiguration
+	Materia toMateria(MateriaDTO materiaDTO);
+	
+	List<MateriaDTO> toMateriaDTOList (List<Materia> materias);
+	
+	List<Materia> toMateriaList(List<MateriaDTO> materiasDTO);
+}
